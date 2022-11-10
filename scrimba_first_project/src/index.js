4f93147f0894d9e1d4ae479daa50646686dabb2f
joe <joe.ulica@gmail.com>
2022-11-10 13:48:59 -0500

    generate react app for first scrimba project

---
+++ scrimba_first_project/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const navbar = (
+  <nav>
+    <h1>Navbar practice</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
+
+root.render(
+  navbar
+);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
