a7dbaea1fd751353d57ea2061b1f47dfde1b48ba
joe <joe.ulica@gmail.com>
2022-11-10 17:18:48 -0500

    start scrimba business card project - add components folder and files

---
+++ digital_business_card/src/Components/Info.js
@@ -0,0 +1,11 @@
+export default function Info() {
+  return (
+    <div id="info">
+      <h1 id="name">Joe Ulica</h1>
+      <h3 id="title">Software Developer</h3>
+      <a href="https://joeu11.github.io/index.html"><p id="website">personal website</p></a>
+      <a href="mailto: joe.ulica@gmail.com"><button id="email">email</button></a>
+      <a href="https://www.linkedin.com/in/joe-ulica/"><button id=" linkedIn">LinkedIn</button></a>
+    </div>
+  )
+}
---
+++ digital_business_card/src/Components/Main.js
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <div>
+      <h2 className="bio-header">About</h2>
+      <p className="bio-text">
+        A junior developer getting his start in the field. Skills include Ruby on Rails, JavaScript, Vue.js, and soon to be React.js.
+      </p>
+      <h2 className="bio-header">Interests</h2>
+      <p className="bio-text">
+        Playing soccer and spending time outdoors with his dogs. Also enjoys a good book or video game, especially in the fantasy genre.
+      </p>
+    </div >
+  )
+}
