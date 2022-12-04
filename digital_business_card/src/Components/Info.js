e7d090bee13d086bec4f8de9feda741ea7cc33ae
joe <joe.ulica@gmail.com>
2022-12-04 18:06:28 -0500

    add navbar - lesson 2

--- digital_business_card/src/Components/Info.js
+++ digital_business_card/src/Components/Info.js
@@ -1,11 +1,14 @@
 export default function Info() {
   return (
     <div id="info">
+      <img src="../Headshot.jpg" alt="headshot" id="headshot"></img>
       <h1 id="name">Joe Ulica</h1>
       <h3 id="title">Software Developer</h3>
-      <a href="https://joeu11.github.io/index.html"><p id="website">personal website</p></a>
-      <a href="mailto: joe.ulica@gmail.com"><button id="email">email</button></a>
-      <a href="https://www.linkedin.com/in/joe-ulica/"><button id=" linkedIn">LinkedIn</button></a>
+      <a href="https://joeu11.github.io/index.html"><p>personal website</p></a>
+      <section id="button-links">
+        <a href="mailto: joe.ulica@gmail.com"><button id="email">email</button></a>
+        <a href="https://www.linkedin.com/in/joe-ulica/"><button id=" linkedIn">LinkedIn</button></a>
+      </section>
     </div>
   )
 }
--- digital_business_card/src/Components/Main.js
+++ digital_business_card/src/Components/Main.js
@@ -1,6 +1,6 @@
 export default function Main() {
   return (
-    <div>
+    <div id="about">
       <h2 className="bio-header">About</h2>
       <p className="bio-text">
         A junior developer getting his start in the field. Skills include Ruby on Rails, JavaScript, Vue.js, and soon to be React.js.
