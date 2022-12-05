2aff4018c3c2102368f6a6e1094d6c32ef00c4e4
joe <joe.ulica@gmail.com>
2022-12-05 18:05:38 -0500

    add Card component

--- lesson_two/src/App.js
+++ lesson_two/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Card />
     </div >
   );
 }
