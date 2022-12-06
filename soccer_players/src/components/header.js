a9a31a02564810ce187bdf0852c6bc849d58f3b5
joe <joe.ulica@gmail.com>
2022-12-05 19:03:14 -0500

    add nav element

---
+++ soccer_players/src/components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav id="nav">
+      <img src="https://image.similarpng.com/very-thumbnail/2020/08/3D-soccer-ball-on-transparent-background-PNG.png" alt="soccer ball" id="nav-logo" />
+      <h1 id="nav-title">Soccer Players App</h1>
+    </nav>
+  )
+}
