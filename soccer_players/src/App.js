fb862604c13d092b05b9431ca98e19eb0791139c
joe <joe.ulica@gmail.com>
2022-12-05 19:20:10 -0500

    hardcode player in player component

--- soccer_players/src/App.js
+++ soccer_players/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/header"
+import Player from "./components/player"
+import axios from 'axios';
+
+// function getData() {
+//   axios.get("http://localhost:3000/players").then(response => (
+//     response = response.data
+//   ))
+// }
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Player />
     </div>
   );
 }
---
+++ soccer_players/src/components/player.js
@@ -0,0 +1,10 @@
+export default function Player() {
+  return (
+    <section className="player">
+      <img src="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcSxbdZSdit4NGau-z7HTdSr17bOFmV4txAy5H-FMcDf6o9pbmUWSSxrbpYGrMqcQcfvs1HYRJhXylYs-f0" alt="Luka Modric" className="player-image" />
+      <h2 className="player-name">Luka Modric</h2>
+      <p>Team: Real Madrid</p>
+      <p>Number: 10</p>
+    </section>
+  )
+}
